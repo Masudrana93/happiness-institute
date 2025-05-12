15d1fa29dd0d3dcb22ded9c3dcfe60e16451e333
Masud Rana <mr007masudrana@gmail.com>
2025-05-12 19:42:14 +0600

    slider breakpoint added

--- swiper-slider.js
+++ swiper-slider.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 10,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -9,8 +9,21 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-    // pagination: {
-    //     el: ".swiper-pagination",
-    //     clickable: true,
-    // },
+
+
+       breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
+    
 });
