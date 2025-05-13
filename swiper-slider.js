dce5715b3630908288b1715f44f023502b4aed03
Masud Rana <mr007masudrana@gmail.com>
2025-05-13 15:18:03 +0600

    public and personality section slider added

--- swiper-slider.js
+++ swiper-slider.js
@@ -1,3 +1,5 @@
+//swiper slider js for english speaking section
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -6,8 +8,8 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
       },
 
 
@@ -27,3 +29,101 @@ var swiper = new Swiper(".mySwiper", {
       },
     
 });
+
+//swiper slider js for public speaking section
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".button-next2",
+        prevEl: ".button-prev2",
+      },
+
+
+       breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
+    
+});
+
+//swiper slider js for Interview session section
+
+var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".button-next3",
+        prevEl: ".button-prev3",
+      },
+
+
+       breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
+    
+});
+
+//swiper slider js for personality development section
+
+var swiper = new Swiper(".mySwiper4", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".button-next4",
+        prevEl: ".button-prev4",
+      },
+
+
+       breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
+    
+});
+
+
